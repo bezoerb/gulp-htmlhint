fdf3ba7376c8539e2584e1ff3d83afbb5bc8941a
Ben Zörb <ben@sommerlaune.com>
2016-01-21 23:24:20 +0100

    bump deps

--- test/main.js
+++ test/main.js
@@ -1,19 +1,17 @@
-/*global describe, it*/
-/*jshint expr: true*/
-"use strict";
+/* eslint-env node, mocha */
+'use strict';
+var fs = require('fs');
+var should = require('should');
+var gutil = require('gulp-util');
+var vfs = require('vinyl-fs');
+var path = require('path');
+var File = gutil.File;
+var HTMLHint = require('htmlhint').HTMLHint;
+var htmlhint = require('../');
 
-var fs = require("fs"),
-    should = require("should"),
-    gutil = require("gulp-util"),
-    vfs = require('vinyl-fs'),
-    path = require('path'),
-    File = gutil.File,
-	HTMLHint = require('htmlhint').HTMLHint,
-    htmlhint = require("../");
+require('mocha');
 
-require("mocha");
-
-var getFile = function(filePath) {
+var getFile = function (filePath) {
     filePath = 'test/' + filePath;
     return new File({
         path: filePath,
@@ -23,20 +21,19 @@ var getFile = function(filePath) {
     });
 };
 
-describe("gulp-htmlhint", function() {
-
-    it("should pass valid file", function(done) {
+describe('gulp-htmlhint', function () {
+    it('should pass valid file', function (done) {
         var valid = 0;
 
-        var fakeFile = getFile("fixtures/valid.html");
+        var fakeFile = getFile('fixtures/valid.html');
 
         var stream = htmlhint();
 
-        stream.on("error", function(err) {
+        stream.on('error', function (err) {
             should.not.exist(err);
         });
 
-        stream.on("data", function(file) {
+        stream.on('data', function (file) {
             should.exist(file);
             file.htmlhint.success.should.equal(true);
             should.exist(file.path);
@@ -45,7 +42,7 @@ describe("gulp-htmlhint", function() {
             ++valid;
         });
 
-        stream.once('end', function() {
+        stream.once('end', function () {
             valid.should.equal(1);
             done();
         });
@@ -54,19 +51,18 @@ describe("gulp-htmlhint", function() {
         stream.end();
     });
 
-
-    it("should fail invalid file", function(done) {
+    it('should fail invalid file', function (done) {
         var invalid = 0;
 
-        var fakeFile = getFile("fixtures/invalid.html");
+        var fakeFile = getFile('fixtures/invalid.html');
 
         var stream = htmlhint();
 
-        stream.on("error", function(err) {
+        stream.on('error', function (err) {
             should.not.exist(err);
         });
 
-        stream.on("data", function(file) {
+        stream.on('data', function (file) {
             should.exist(file);
             file.htmlhint.success.should.equal(false);
             file.htmlhint.errorCount.should.equal(1);
@@ -77,7 +73,7 @@ describe("gulp-htmlhint", function() {
             ++invalid;
         });
 
-        stream.once('end', function() {
+        stream.once('end', function () {
             invalid.should.equal(1);
             done();
         });
@@ -86,18 +82,18 @@ describe("gulp-htmlhint", function() {
         stream.end();
     });
 
-    it('should lint two files', function(done) {
+    it('should lint two files', function (done) {
         var a = 0;
 
-        var file1 = getFile("fixtures/valid.html");
-        var file2 = getFile("fixtures/invalid.html");
+        var file1 = getFile('fixtures/valid.html');
+        var file2 = getFile('fixtures/invalid.html');
 
         var stream = htmlhint();
-        stream.on('data', function() {
+        stream.on('data', function () {
             ++a;
         });
 
-        stream.once('end', function() {
+        stream.once('end', function () {
             a.should.equal(2);
             done();
         });
@@ -107,7 +103,7 @@ describe("gulp-htmlhint", function() {
         stream.end();
     });
 
-    it('should support options', function(done) {
+    it('should support options', function (done) {
         var a = 0;
 
         var file = getFile('fixtures/invalid.html');
@@ -115,14 +111,14 @@ describe("gulp-htmlhint", function() {
         var stream = htmlhint({
             'tag-pair': false
         });
-        stream.on('data', function(newFile) {
+        stream.on('data', function (newFile) {
             ++a;
             should.exist(newFile.htmlhint.success);
             newFile.htmlhint.success.should.equal(true);
             should.not.exist(newFile.htmlhint.results);
             should.not.exist(newFile.htmlhint.options);
         });
-        stream.once('end', function() {
+        stream.once('end', function () {
             a.should.equal(1);
             done();
         });
@@ -131,20 +127,20 @@ describe("gulp-htmlhint", function() {
         stream.end();
     });
 
-    it('should support htmlhintrc', function(done) {
+    it('should support htmlhintrc', function (done) {
         var a = 0;
 
         var file = getFile('fixtures/invalid.html');
 
         var stream = htmlhint('test/htmlhintrc.json');
-        stream.on('data', function(newFile) {
+        stream.on('data', function (newFile) {
             ++a;
             should.exist(newFile.htmlhint.success);
             newFile.htmlhint.success.should.equal(true);
             should.not.exist(newFile.htmlhint.results);
             should.not.exist(newFile.htmlhint.options);
         });
-        stream.once('end', function() {
+        stream.once('end', function () {
             a.should.equal(1);
             done();
         });
@@ -153,7 +149,7 @@ describe("gulp-htmlhint", function() {
         stream.end();
     });
 
-    it('should emit error on failure', function(done) {
+    it('should emit error on failure', function (done) {
         var file = getFile('fixtures/invalid.html');
 
         var stream = htmlhint();
@@ -161,7 +157,7 @@ describe("gulp-htmlhint", function() {
         var failStream = htmlhint.reporter('fail');
         stream.pipe(failStream);
 
-        failStream.on('error', function(err) {
+        failStream.on('error', function (err) {
             should.exist(err);
             err.message.indexOf(file.relative).should.not.equal(-1, 'should say which file');
             done();
@@ -171,109 +167,104 @@ describe("gulp-htmlhint", function() {
         stream.end();
     });
 
-	it('should add a htmlhint rule', function() {
-		var fakeRule = {
-			id: 'foo',
-			description: 'bar',
-			init: function() {}
-		};
-		htmlhint.addRule(fakeRule);
-		HTMLHint.rules[fakeRule.id].should.equal(fakeRule);
-	});
-
+    it('should add a htmlhint rule', function () {
+        var fakeRule = {
+            id: 'foo',
+            description: 'bar',
+            init: function () {}
+        };
+        htmlhint.addRule(fakeRule);
+        HTMLHint.rules[fakeRule.id].should.equal(fakeRule);
+    });
 });
 
-describe('htmlhint.reporter', function() {
-
-    it('should not fail for more than 16 files', function(done) {
+describe('htmlhint.reporter', function () {
+    it('should not fail for more than 16 files', function (done) {
         var a = 0;
 
         var stream = vfs.src('test/fixtures/morethan16/*.html')
             .pipe(htmlhint())
-            .pipe(htmlhint.reporter(function(){
+            .pipe(htmlhint.reporter(function () {
                 a++;
             }));
 
+        stream.on('data', function () {
+        });
 
-        stream.on('data', function() {});
-
-        stream.once('end', function() {
+        stream.once('end', function () {
             a.should.equal(17);
             done();
         });
     });
 
-	it('should load custom reporters by package name', function(done) {
-		var valid = 0;
+    it('should load custom reporters by package name', function (done) {
+        var valid = 0;
 
         var stream = vfs.src('test/fixtures/valid.html')
-	        .pipe(htmlhint())
-	        .pipe(htmlhint.reporter('htmlhint-stylish'));
+            .pipe(htmlhint())
+            .pipe(htmlhint.reporter('htmlhint-stylish'));
 
-		stream.on('error', function(err) {
-			should.not.exist(err);
-		});
+        stream.on('error', function (err) {
+            should.not.exist(err);
+        });
 
-		stream.on('data', function(file) {
-			should.exist(file);
-			file.htmlhint.success.should.be.true;
+        stream.on('data', function (file) {
+            should.exist(file);
+            /* eslint no-unused-expressions: 0 */
+            file.htmlhint.success.should.be.true;
             should.exist(file.path);
             should.exist(file.relative);
             should.exist(file.contents);
             ++valid;
-		});
+        });
 
-	    stream.once('end', function() {
-			valid.should.equal(1);
-	        done();
-	    });
-	});
-
+        stream.once('end', function () {
+            valid.should.equal(1);
+            done();
+        });
+    });
 });
 
-describe('htmlhint.errorreporter', function(){
-    it('should throw an error when using on an invalid file', function(done){
+describe('htmlhint.errorreporter', function () {
+    it('should throw an error when using on an invalid file', function (done) {
         var error = false;
         var stream = vfs.src('test/fixtures/invalid.html')
             .pipe(htmlhint())
             .pipe(htmlhint.failReporter());
 
-
-
-        stream.on('error', function(err){
+        stream.on('error', function (err) {
             error = true;
-            gutil.colors.stripColor(err.message).should.containEql('[L9:C1] Tag must be paired, Missing: [ </h1> ] (tag-pair)');
+            gutil.colors.stripColor(err.message).should.containEql('[L9:C1] Tag must be paired, missing: [ </h1> ], start tag match failed [ <h1> ]');
             err.name.should.equal('Error');
             done();
         });
 
-        stream.once('end', function() {
+        stream.once('end', function () {
+            /* eslint no-unused-expressions: 0 */
             error.should.be.true;
             done();
         });
     });
 
-    it('should not show file errors if suppress option is explicitly set', function(done){
+    it('should not show file errors if suppress option is explicitly set', function (done) {
         var error = false;
         var stream = vfs.src('test/fixtures/invalid.html')
             .pipe(htmlhint())
             .pipe(htmlhint.failReporter({
-				suppress: true
+                suppress: true
             }));
 
-
-
-        stream.on('error', function(err){
+        stream.on('error', function (err) {
             error = true;
             gutil.colors.stripColor(err.message).should.containEql('HTMLHint failed.');
             err.name.should.equal('Error');
             done();
         });
 
-        stream.once('end', function() {
+        stream.once('end', function () {
+            /* eslint no-unused-expressions: 0 */
             error.should.be.true;
             done();
         });
     });
-
 });
